6187f89090e22c7b38092c707a64bda7c2e82632
Dzmitry Dzemidziuk <extern.dzemidziuk_dzmitry@allianz.com>
2024-05-07 22:46:51 +0200

    added sending sms

--- app/src/main/java/com/dimadyuk/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/dimadyuk/dogs/view/DetailFragment.kt
@@ -1,33 +1,48 @@
 package com.dimadyuk.dogs.view
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import com.dimadyuk.dogs.R
 import com.dimadyuk.dogs.databinding.FragmentDetailBinding
+import com.dimadyuk.dogs.databinding.SendSmsDialogBinding
+import com.dimadyuk.dogs.model.DogBreed
 import com.dimadyuk.dogs.model.DogPalette
+import com.dimadyuk.dogs.model.SmsInfo
 import com.dimadyuk.dogs.viewmodel.DetailsViewModel
 
 class DetailFragment : Fragment() {
 
     private lateinit var viewModel: DetailsViewModel
+    private var dogUuid = 0
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
-    private var dogUuid = 0
+    private var sendSMSStarted = false
+    private var currentDog: DogBreed? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
@@ -44,8 +59,69 @@ class DetailFragment : Fragment() {
         observeViewModel()
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_send_sms -> {
+                sendSMSStarted = true
+                (activity as MainActivity).checkSmsPermission()
+            }
+            R.id.action_share -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onPermissionResult(permissionGranted: Boolean) {
+        if (sendSMSStarted && permissionGranted) {
+            context?.let {
+                val smsInfo =
+                    SmsInfo(
+                        "",
+                        "${currentDog?.dogBread} bred for ${currentDog?.breedFor}. Image - ${currentDog?.imageUrl}",
+                        currentDog?.imageUrl
+                    )
+
+                val dialogBinding = DataBindingUtil
+                    .inflate<SendSmsDialogBinding>(
+                        LayoutInflater.from(it),
+                        R.layout.send_sms_dialog,
+                        null,
+                        false
+                    )
+                AlertDialog.Builder(it)
+                    .setView(dialogBinding.root)
+                    .setPositiveButton("Send SMS") { dialog, which ->
+                        if (!dialogBinding.smsDestination.text.isNullOrEmpty()) {
+                            smsInfo.to = dialogBinding.smsDestination.text.toString()
+                            sendSms(smsInfo)
+                        }
+                    }
+                    .setNegativeButton("Cancel") { dialog, which -> }
+                    .show()
+
+                dialogBinding.smsInfo = smsInfo
+            }
+        }
+    }
+
+    private fun sendSms(smsInfo: SmsInfo) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(smsInfo.to, null, smsInfo.text, pendingIntent, null)
+    }
+
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(viewLifecycleOwner) { dogBreed ->
+            currentDog = dogBreed
             with(binding) {
                 dogBreed?.let {
                     dog = dogBreed
--- app/src/main/java/com/dimadyuk/dogs/view/MainActivity.kt
+++ app/src/main/java/com/dimadyuk/dogs/view/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.dimadyuk.dogs.view
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
@@ -11,6 +15,7 @@ import androidx.navigation.ui.NavigationUI
 import com.dimadyuk.dogs.R
 import com.dimadyuk.dogs.databinding.ActivityMainBinding
 import com.dimadyuk.dogs.util.NotificationsHelper
+import com.dimadyuk.dogs.util.PERMISSION_SEND_SMS
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,9 +43,59 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         notificationsHelper.handlePermissionResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_SEND_SMS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    notifyDetailFragment(true)
+                } else {
+                    notifyDetailFragment(false)
+                }
+            }
+        }
+    }
+
+    private fun notifyDetailFragment(permissionGranted: Boolean) {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val activeFragment = navHostFragment.childFragmentManager.fragments.firstOrNull()
+        if (activeFragment is DetailFragment) {
+            activeFragment.onPermissionResult(permissionGranted)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, null)
+    }
+
+    fun checkSmsPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.SEND_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.SEND_SMS)) {
+                AlertDialog.Builder(this)
+                    .setTitle("Send SMS permission")
+                    .setMessage("This app requires access to send an SMS.")
+                    .setPositiveButton("Ask me") { dialog, which ->
+                        requestSmsPermission()
+                    }
+                    .setNegativeButton("No") { dialog, which ->
+                        notifyDetailFragment(false)
+                    }
+                    .show()
+            } else {
+                requestSmsPermission()
+            }
+        } else {
+            notifyDetailFragment(true)
+        }
+    }
+
+    private fun requestSmsPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.SEND_SMS),
+            PERMISSION_SEND_SMS
+        )
     }
 }
